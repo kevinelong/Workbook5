f86bd920f50d68052c9be8dc32b8dbe5ddf9c166
kevinelong <kevinelong@gmail.com>
2022-10-19 08:55:21 -0700

    update

--- Exercises/MultiArrayScripts/food_menu_array.js
+++ Exercises/MultiArrayScripts/food_menu_array.js
@@ -12,12 +12,12 @@ let menu = [
         { item: "Bacon and Egg Biscuit", price: 3.49 },
         { item: "Ham and Egg Biscuit", price: 3.29 }
     ],
-    [ /* lunch items */
+    [
         { item: "burger", price: 5.36 },
         { item: "fries", price: 3.00 },
         { item: "shake", price: 8.37 },
     ],
-    [ /* dinner items */
+    [
         { item: "steak", price: 27.23 },
         { item: "pork chop", price: 12.27 },
         { item: "mashed potatoes", price: 2.00 },
@@ -33,7 +33,8 @@ const DINNER = 2;
 const TITLES = ["breakfast items", "lunch items", "dinner items"];
 
 function showItem(item) {
-    console.log("\t", item.price, "\t", item.item) //USE TABS (\t) TO LINE THEM UP
+    //USE TABS (\t) TO LINE THEM UP
+    console.log("\t", item.price, "\t", item.item)
 }
 
 function showItemsForMealTime(meal) {
@@ -41,7 +42,7 @@ function showItemsForMealTime(meal) {
     console.log(TITLES[meal]);
 
     // show the items for the selected meal
-    menu[meal].forEach(showItem);
+    menu[meal].forEach(showItem); //THIS IS THE KEY PART OF THE LONG ANSWER
 }
 
 let meal = BREAKFAST;  //  0==BREAKFAST
